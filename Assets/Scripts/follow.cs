95fd90e24e59ae2067afa57c0bf61b6747dce0bb
Anders <anders1603@hotmail.com>
2020-09-02 07:50:44 +0200

    door

--- Assets/Scripts/follow.cs
+++ Assets/Scripts/follow.cs
@@ -16,7 +16,7 @@ public class Follow : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 	rb.MovePosition(target.transform.position);
         
