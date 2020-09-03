e8b8d0af8ad9b030bcefd2ae8414ac1fbdded6fb
Dominoeffekten <rikkedomino@gmail.com>
2020-09-03 11:00:15 +0200

    ødelæg møbler

---
+++ Assets/Scripts/BrokenItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// https://www.youtube.com/watch?v=xqQkiHI5U9o
+
+public class BrokenItem : MonoBehaviour
+{
+    //makes a compoment 
+    public GameObject replacement;
+
+    void OnCollisionEnter(){
+
+        //an extising object that you want to make a copy of
+        GameObject.Instantiate(replacement, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
